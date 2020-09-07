077643a343caabbff85f1a5c22fc1bc085e44572
Fern1ck <52502935+Fern1ck@users.noreply.github.com>
2020-09-06 23:54:32 -0300

    Updated the python services

--- services/py/password_funcs.py
+++ services/py/password_funcs.py
@@ -1,10 +1,13 @@
 from hashlib import sha256
 import re
+import json
+
+errores = json.load(open("../../error/errores.json", "r")) #Error codes
 
 def passwd_hash(passwd):
     #Verify if the password is somewhat secure
     if re.fullmatch(r'[A-Za-z0-9@#$%^&+=]{8,}', passwd):
-        #Devolver su hash
+        #Encrypt the password with SHA256
         h = sha256()
         h.update(passwd.encode('utf-8'))
         return h.hexdigest()
